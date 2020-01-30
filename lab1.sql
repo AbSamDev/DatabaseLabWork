8621d296449dec1f1ef431a13af9431b41fe7ca4
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2020-01-30 22:17:23 +0500

    uploaded lab1
    1st week task of database with lab1.

---
+++ lab1.sql
@@ -0,0 +1,22 @@
+/* ;+-------------------------------------------+
+  ;|                   Database
+  ;|   ========================================
+  ;|   DataBase Lab
+  ;|   Week 01
+  ;|   ========================================
+  ;|   Created by: Abdul Samad
+  ;|   Registration No: 2018-CS-141
+  ;|   ========================================
+  ;+-------------------------------------------+ */
+
+/* Need to install and connect SQL server management studio 
+
+
+CREATE DATABASE LAB1  /* creation of a database named lab1 */
+
+DROP DATABASE LAB1    /* DELETION of a database named lab1 */
+
+
+
+/* SQl is a 4rth generation language like other scripting languages */
+
