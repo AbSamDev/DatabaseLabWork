4f3d32171a326f8cfc762602a21db5419e5f1a70
Abdul Samad <53661372+abdulsamad-optimisticstr@users.noreply.github.com>
2020-02-06 13:42:07 +0500

    craeting lab3

---
+++ lab3.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE LAB3
+USE LAB3
+
+/* creating table named student */
+
+CREATE TABLE student(          
+                               /* datatypes: int, char, varchar */
+roll char (13) primary key,    /* primary key is such an attribute use to uniquely identify each rowin a database */
+student_name varchar (50),     /* varchar take dynamic space */
+student_address varchar (50),
+age int                        /* no need of comma on ending attribute of the table*/
+)
+
+/* inserting data in students table */
+insert into student( roll,  student_name, student_address, age) values ('141', 'Abdul Samad', 'Sialkot', 19)
+insert into student( roll, student_name, student_address) values ('140', 'Nabeel', 'Garhi sho') 
+insert into student( roll, student_name) values ('142', 'Sadam') 
+insert into student( roll, student_name, age) values ('143', 'Ahsan', 104) 
+insert into student( roll, student_name) values ('144', 'Rana Shaukat') 
+insert into student( roll, student_name) values ('145', 'Barry') 
+insert into student( roll, student_name) values ('147', 'Safder') 
+
+
+SELECT * FROM STUDENT
+/* ADDING AN ATTRIBUTE(COLUMN) */
+ALTER TABLE STUDENT ADD CONTACTS VARCHAR(20)
+/* ADDING TWO (OR MORE) ATTRIBUTE(COLUMN) AT A TIME */
+ALTER TABLE STUDENT ADD NOCONTACTS VARCHAR(20), dOB DATE
+/* DELETING ATTRIBUTES(COLUMNS) */
+ALTER TABLE STUDENT DROP COLUMN CONTACTS, sob
+/* CHANGING DATATYPE OF ATTRIBUTE(COLUMN) */
+ALTER TABLE STUDENT ALTER COLUMN dob VARCHAR(20)
